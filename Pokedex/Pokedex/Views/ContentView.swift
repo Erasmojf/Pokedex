cf3d743cdcd4288528238e26ce829ec22cc49ee0
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2022-06-24 17:10:14 -0300

    Finalizando app pokedex
    Finalizando app pokedex

--- Pokedex/Pokedex/Views/ContentView.swift
+++ Pokedex/Pokedex/Views/ContentView.swift
@@ -11,12 +11,27 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var vm = ViewModel()
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .padding()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 10) {
+                    ForEach(vm.filteredPokemon) { pokemon in
+                        NavigationLink(destination: PokemonDetailView(pokemon: pokemon)) {
+                            PokemonView(pokemon: pokemon)
+                        }
+                        
+                    }
+                }
+                .animation(.easeIn(duration: 0.3), value: vm.filteredPokemon.count)
+                .navigationTitle("Pokemons")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .searchable(text: $vm.searchText)
         }
-
+        .environmentObject(vm)
     }
 }
 
